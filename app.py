86198bdf1d2c4973491d7d66bef1cdebf882b6ef
draj2931 <draj293@gmail.com>
2021-05-07 11:55:49 +0800

    adding pages

--- app.py
+++ app.py
@@ -1,7 +1,22 @@
-from flask import Flask
+from flask import Flask,render_template
+from flask_sqlalchemy import SQLAlchemy 
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route('/')
+@app.route('/index')
 def index():
-    return "test"
+    return  render_template('index.html')
+
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')  
+
+
+if __name__=="__main__":
+    app.run(debug=True)
+
